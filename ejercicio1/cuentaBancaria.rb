b9e3c248c18867f749052347f077a3c5b32f8815
Tomas Briones <tbriones@economial.cl>
2021-04-17 18:48:44 -0400

    initialize cuentaBancaria

--- ejercicio1/cuentaBancaria.rb
+++ ejercicio1/cuentaBancaria.rb
@@ -1,5 +1,11 @@
 class CuentaBancaria
-    def initialize
-        
+    
+    attr_accessor :saldo
+    attr_reader :nombreBanco, :numeroCuenta
+
+    def initialize(nombreBanco,numeroCuenta,saldo=0)
+        @nombreBanco = nombreBanco
+        @numeroCuenta = numeroCuenta
+        @saldo = saldo
     end
 end
