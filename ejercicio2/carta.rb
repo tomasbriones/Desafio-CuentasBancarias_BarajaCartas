9513410f315ac16c82777d80720aea966f87ce7b
Tomas Briones <tbriones@economial.cl>
2021-04-18 00:46:23 -0400

    deleting comments in carta

--- ejercicio2/carta.rb
+++ ejercicio2/carta.rb
@@ -1,20 +1,12 @@
 class Carta
     attr_reader :numero, :pinta
-    #inicializo el metodo constructor con las variables que tengo que definir.
     def initialize(numero, pinta)
-
-        #pinta.upcase!
-        #pinta.to_s!
         pintas = ['C','D','E','T']
-
-        #evaluamos si esta entre el rango correcto, evaluamos que la pinta sea la correcta
         if (numero >= 1 && numero <= 13) && pintas.include?(pinta)
             @numero = numero
             @pinta = pinta
         else
-            raise ArgumentError.new("#{pinta} No es un argumento válido") #levanta una exepcion del tipo ArgumentError
+            raise ArgumentError.new("#{pinta} No es un argumento válido") 
         end
     end
-    #'f', pinta que no existe, entra igual al initialize
-    #require_relative 'desafio1_cuentabancaria/cartas'
 end
